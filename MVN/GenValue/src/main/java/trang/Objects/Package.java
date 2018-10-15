f6b70b13045b446b153b4a1f73db0d7eda7fc2c1
Le Quynh Trang <lequynhtrang1202961@gmail.com>
2018-10-15 17:51:01 +0700

    set up object

--- MVN/GenValue/src/main/java/trang/Objects/Package.java
+++ MVN/GenValue/src/main/java/trang/Objects/Package.java
@@ -3,16 +3,16 @@ package trang.Objects;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Values  {
+public class Package  {
 	private String packagename;
 	private String classname;
 	private List<Method> methodlist = new ArrayList<>();
 	
-	public Values() {
+	public Package() {
 		
 	}
 	
-	public Values(String packagename, String classname, List<Method> methodnamelist) {
+	public Package(String packagename, String classname, List<Method> methodnamelist) {
 		this.packagename = packagename;
 		this.classname = classname; 
 		this.methodlist = methodnamelist;
@@ -41,6 +41,15 @@ public class Values  {
 	
 	public Method getAMethod(int index) {
 		return this.methodlist.get(index);
+	}
+	
+	public List<Method> getMethodList(){
+		return this.methodlist;
+	}
+	
+	public boolean methodlistIsNull() {
+		if(methodlist.get(0) == null) return true;
+		else return false;
 	}
 	
 }
